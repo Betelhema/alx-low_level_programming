a7357db4594b748038ced2fc8bc7c24277b4d5a9
betelhema <betelhemayalew27@gmail.com>
2022-10-13 03:51:28 -0700

    less

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main:Prints 
- *Return:Always 0
+ * main - entry
+ * Return: Always 0.
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+return (0);
 }
-
