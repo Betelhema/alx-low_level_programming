30f28508806b1a2cbeeadfe2d76ed8a554aee8e8
betelhema <betelhemayalew27@gmail.com>
2022-10-14 06:13:17 -0700

    ll

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main -entry
- *
- * Returns : always 0 (success)
- */
-intmain(void)
+*main - entry
+*Returns : always 0 (success)
+*/
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
