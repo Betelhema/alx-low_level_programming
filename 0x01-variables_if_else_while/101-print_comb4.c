da69f389bc62cc11282507507acabfdb55a77a54
betelhema <betelhemayalew27@gmail.com>
2022-10-14 05:10:30 -0700

    kk

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,11 +14,11 @@ for (i = '0'; i <= '9'; i++)
 for (k = '0'; k <= '9'; k++)
 {
 if (c < i && i < k)
-{		  
+{
 putchar(c);
 putchar(i);
 putchar(k);
-if (c != '7')
+if (c != '7'
 {
 putchar(',');
 putchar(' ');
