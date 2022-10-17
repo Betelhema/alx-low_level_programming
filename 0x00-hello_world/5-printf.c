2d86fa42e38867241848c6132442d63f4f270782
betelhema <betelhemayalew27@gmail.com>
2022-10-17 00:56:03 -0700

    tew

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*main - entry
-*
-*Returns : always 0 (success)
+*Description: main - Prints out a string to stdout.
+*Return: 0 if success.
 */
-intmain(void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
+
+
