c9aa77ce12ed3e328c35ba9a68f620caf85f58bf
betelhema <betelhemayalew27@gmail.com>
2022-10-18 08:14:05 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,17 @@
-#ifndef main.h
-#define main.h
-char _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+char_putchar(char c);
+voidprint_alphabet(void);
+voidprint_alphabet_x10(void);
+int_islower(int c);
+int_isalpha(int c);
+intprint_sign(int n);
+int_abs(int);
+intprint_last_digit(int);
+voidjack_bauer(void);
+voidtimes_table(void);
+intadd(int, int);
+voidprint_to_98(int n);
+voidprint_times_table(int n);
+
 
 
 
