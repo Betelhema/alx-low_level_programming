bdcd5be92b5c161e783abf37b0f5723e6e233023
betelhema <betelhemayalew27@gmail.com>
2022-10-18 07:26:36 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef main.h
 #define main.h
-int _putchar(char c);
+char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,8 +14,6 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 #endif /* main.h */
-
-
 
 
 
