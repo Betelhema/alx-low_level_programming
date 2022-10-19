ba94d38a5a9dea4f0ffa4712de30595006eb3bfe
betelhema <betelhemayalew27@gmail.com>
2022-10-19 02:41:39 -0700

    ll

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * jack_bauer -  function that prints every minute of the day of Jack Bauer
 * starting from 00:00 to 23:59.
@@ -24,5 +24,3 @@ j += 1;
 x += 1;
 }
 }
-
-
