ec666f785b12c9eaf194fec3850140f3de196205
betelhema <betelhemayalew27@gmail.com>
2022-10-24 09:05:08 -0700

    kk

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,6 +9,7 @@ int _atoi(char *s)
 int i = 0;
 int n = 0;
 int signo = 1;
+}
 while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 {
 if (s[i] == '-');
@@ -31,8 +32,3 @@ i++;
 signo *= -1;
 return (n * signo);
 }
-
-
-
-
-
